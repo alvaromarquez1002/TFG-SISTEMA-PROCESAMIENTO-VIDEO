8691981bb5a796e74556c48b09bf19be45cb1b28
amq1002 <amq1002@alu.ubu.es>
2025-06-28 14:53:35 +0200

    fix: Intento de solución forzando URLs en config.js

--- configuracion_despliegue/jitsi/gen-passwords.sh
+++ configuracion_despliegue/jitsi/gen-passwords.sh
@@ -11,6 +11,17 @@ JIBRI_RECORDER_PASSWORD=$(generatePassword)
 JIBRI_XMPP_PASSWORD=$(generatePassword)
 JIGASI_TRANSCRIBER_PASSWORD=$(generatePassword)
 
+echo "################################################################################"
+echo "Las contraseñas generadas son (copia y pega estas en tu archivo .env):"
+echo "################################################################################"
+echo "JICOFO_AUTH_PASSWORD=${JICOFO_AUTH_PASSWORD}"
+echo "JVB_AUTH_PASSWORD=${JVB_AUTH_PASSWORD}"
+echo "JIGASI_XMPP_PASSWORD=${JIGASI_XMPP_PASSWORD}"
+echo "JIBRI_RECORDER_PASSWORD=${JIBRI_RECORDER_PASSWORD}"
+echo "JIBRI_XMPP_PASSWORD=${JIBRI_XMPP_PASSWORD}"
+echo "JIGASI_TRANSCRIBER_PASSWORD=${JIGASI_TRANSCRIBER_PASSWORD}"
+echo "################################################################################"
+
 #comento la siguiente sección de 'sed' para evitar problemas de permisos al ejecutar el script
 #sed -i.bak \
 #    -e "s#JICOFO_AUTH_PASSWORD=.*#JICOFO_AUTH_PASSWORD=${JICOFO_AUTH_PASSWORD}#g" \
